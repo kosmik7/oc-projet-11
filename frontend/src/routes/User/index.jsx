3ad3bf7cb9963503e368eb96191831278c5a505b
JP Brunache <kosmik7@users.noreply.github.com>
2023-06-03 17:55:29 +0200

    Ajout du formulaire pour éditer le 'username'

--- frontend/src/routes/User/index.jsx
+++ frontend/src/routes/User/index.jsx
@@ -1,15 +1,85 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { editUsername } from "../../features/user/userAPI";
+
 import "./style.scss";
+
+function Input({ type, label, ...props }) {
+  const id = label.replace(/\s+/g, "").toLowerCase();
+  return (
+    <>
+      <label htmlFor={id}>{label}:</label>
+      <input type="text" id={id} name={id} size="15" {...props} />
+    </>
+  );
+}
+
+function EditUserForm() {
+  const dispatch = useDispatch();
+  const { loading, token, data } = useSelector((state) => state.user);
+  const [isFormVisible, setFormVisible] = useState(false);
+  const [username, setUsername] = useState("");
+
+  function handleEditButton() {
+    setFormVisible(!isFormVisible);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!loading) {
+      dispatch(editUsername({ token, username }));
+    }
+    setFormVisible(!isFormVisible);
+  };
+
+  return (
+    <>
+      {isFormVisible ? (
+        <>
+          <h1>Edit user info</h1>
+          <form className="editUserForm" onSubmit={(e) => handleSubmit(e)}>
+            <Input
+              label={"User name"}
+              placeholder={data?.userName}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <Input
+              label={"First name"}
+              placeholder={data?.firstName}
+              disabled
+            />
+            <Input label={"Last name"} placeholder={data?.lastName} disabled />
+            <div className="edit-button-container">
+              <button className="edit-button" type="submit">
+                Save
+              </button>
+              <button className="edit-button" onClick={handleEditButton}>
+                Cancel
+              </button>
+            </div>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1>
+            Welcome back
+            <br /> {data?.firstName} {data?.lastName}!
+          </h1>
+          <button className="edit-button" onClick={handleEditButton}>
+            Edit Name
+          </button>
+        </>
+      )}
+    </>
+  );
+}
 
 export default function User() {
   return (
     <main className="main bg-dark">
       <div className="header">
-        <h1>
-          Welcome back
-          <br />
-          Tony Jarvis!
-        </h1>
-        <button className="edit-button">Edit Name</button>
+        <EditUserForm />
       </div>
       <h2 className="sr-only">Accounts</h2>
       <section className="account">
