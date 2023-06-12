d6571e2b71b4f816dba6b0bddfb43731ed3ab0f5
JP Brunache <kosmik7@users.noreply.github.com>
2023-06-12 12:29:52 +0200

    Optimisation composant 'Feature'

--- frontend/src/components/Features/index.jsx
+++ frontend/src/components/Features/index.jsx
@@ -1,42 +1,35 @@
 import "./style.scss";
 
+function Feature({ icon, alt, heading, paragraph }) {
+  return (
+    <div className="feature-item">
+      <img src={icon} alt={alt} className="feature-icon" />
+      <h3 className="feature-item-title">{heading}</h3>
+      <p>{paragraph}</p>
+    </div>
+  );
+}
 export default function Features() {
   return (
     <section className="features">
-      <h2 className="sr-only">Features</h2>
-      <div className="feature-item">
-        <img
-          src="./img/icon-chat.png"
-          alt="Chat Icon"
-          className="feature-icon"
-        />
-        <h3 className="feature-item-title">You are our #1 priority</h3>
-        <p>
-          Need to talk to a representative? You can get in touch through our
-          24/7 chat or through a phone call in less than 5 minutes.
-        </p>
-      </div>
-      <div className="feature-item">
-        <img
-          src="./img/icon-money.png"
-          alt="Chat Icon"
-          className="feature-icon"
-        />
-        <h3 className="feature-item-title">More savings means higher rates</h3>
-        <p>The more you save with us, the higher your interest rate will be!</p>
-      </div>
-      <div className="feature-item">
-        <img
-          src="./img/icon-security.png"
-          alt="Chat Icon"
-          className="feature-icon"
-        />
-        <h3 className="feature-item-title">Security you can trust</h3>
-        <p>
-          We use top of the line encryption to make sure your data and money is
-          always safe.
-        </p>
-      </div>
+      <Feature
+        icon="./img/icon-chat.png"
+        alt="Chat Icon"
+        heading="You are our #1 priority"
+        paragraph="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
+      />
+      <Feature
+        icon="./img/icon-money.png"
+        alt="Money Icon"
+        heading="More savings means higher rates"
+        paragraph="The more you save with us, the higher your interest rate will be!"
+      />
+      <Feature
+        icon="./img/icon-security.png"
+        alt="Security Icon"
+        heading="Security you can trust"
+        paragraph="We use top of the line encryption to make sure your data and money is always safe."
+      />
     </section>
   );
 }
